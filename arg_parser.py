d846afaa43d5747b6bd1e3b9d12e4582a05f816a
fodui <nikolov7856@mail.ru>
2025-12-18 15:25:01 +0700

    add cli options

---
+++ arg_parser.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+path_to_file_history = os.getenv("PATH_TO_FILE_HISTORY")
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+
+def add_cli_options():
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "-hs", "--host",
+        help="indicate host",
+        default=host
+    )
+    
+    parser.add_argument(
+        '-p', '--port',
+        help="indicate port",
+        default=port
+    )
+    
+    parser.add_argument(
+        '-ph', '--path_history',
+        help="indicate path to history file",
+        default=path_to_file_history
+    )
+    
+    return host, port, path_to_file_history
--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from time import strftime
 import aiofiles
 import datetime
 
+from arg_parser import add_cli_options
+
+host, port, path_file_history = add_cli_options()
 
 #formatted_date_3 = now.strftime("%Y-%m-%d-%H.%M.%S")
 async def mine_chat():
     reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000)
+        host, port)
 
     while True:
         current_data = datetime.datetime.now()
@@ -18,7 +21,7 @@ async def mine_chat():
         data_chat = await reader.read(200)
         new_message = f'[{now_strftime}] {data_chat.decode()}\n'
         
-        async with aiofiles.open('chat_messages.txt', "a", encoding='utf-8') as file:
+        async with aiofiles.open(path_file_history, "a", encoding='utf-8') as file:
             await file.write(new_message)
         
         print(new_message)
